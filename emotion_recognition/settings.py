bf6a9d8df554048af1603368fd5702ae7e8bd976
satya615 <vsatyanarayana@gmail.com>
2024-10-14 16:16:45 +0530

    initial

--- emotion_recognition/settings.py
+++ emotion_recognition/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-PYTHON_VERSION = '3.10.0'
+PYTHON_VERSION = os.getenv('PYTHON_VERSION', '3.10.4')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
