03f1f2a0d796badbba504277680eec2e0a638a74
satya615 <vsatyanarayana@gmail.com>
2024-10-18 09:54:36 +0530

    build.sh

--- build_files.sh
+++ build_files.sh
@@ -4,8 +4,6 @@ echo " BUILD START"
 
 # Use 'python3' instead of 'python3.10' since Vercel might not have Python 3.10 installed by default
 python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
-
 # Ensure the output directory exists and is correctly handled
 mkdir -p staticfiles_build
 mv static/* staticfiles_build/
